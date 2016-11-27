88d7a323b80b3b1013b8dd712cdc654ee24142c9
alex <alex.vlissidis@gmail.com>
2016-11-27 20:10:42 +0000

    New breeding method

--- genetics.py
+++ genetics.py
@@ -1,41 +1,46 @@
-from nnmath import *
+import copy
 from random import randint
 
 # Gene Super Class
 class Gene:
 	fitness = 0
 	score = 0
+	genotype = ""
 
 	def __init__(self):
 		pass
 
-	def mutate(self):
-		pass
-
-	def breed(self, gene):
-		pass
-
 	def encode(self):
 		pass
+
+	def decode(self):
+		pass
+
+	def mutate(self, rate):
+		gen_len = len(self.genotype)
+
+		for _ in round(rate * gen_len)
+			idx = randint(0, gen_len) # Select a random bit
+			# Flip the bit
+			genotype[idx] = '0' if genotype[randint(0, gen_len)] == '1' else '1'
 
 	def evaluate(self):
 		pass
 
 
 class GeneticAlgorithm:
-	def __init__(self, error, mutation_rate, obj, training_data, targets):
+	def __init__(self, error, mutation_rate, data, targets, obj, args):
 		self.obj = obj
 		self.mutation_rate = mutation_rate
 		self.target_error = error
-		self.training_data = training_data
-		self.imglen = len(training_data[0][1]) # Use first image in set
+		self.training_data = data
 		self.targets = targets
 		self.popsize = 0
 		self.error = 1
 
 	def population(self, size):
 		# Use the factory method to create the population
-		self.population = [self.obj(self.imglen) for i in range(size)]
+		self.population = [self.obj(args) for i in range(size)]
 		self.popsize = size
 
 	def evaluate(self):
@@ -44,23 +49,30 @@ class GeneticAlgorithm:
 			gene.score = 0
 			for data in self.training_data:
 				(tags, input_vector) = data
+				print tags
 
 				output = gene.evaluate(input_vector)
+				print output
 				target = map(lambda x: int(x in tags), self.targets)
 				eval_vector = (target == np.around(output)).astype(np.int)
 
-				gene.score += np.sum(np.multiply(output, eval_vector))
+				scorer = [0] * len(output)
+				for i, value in enumerate(target):
+					if value == 0:
+						scorer[i] = 1 - output[i]
+					else:
+						scorer[i] = output[i]
+
+				gene.score += np.dot(scorer, eval_vector)
 
 			gene.fitness = gene.score/(len(eval_vector) * len(self.training_data))
+			print "------------------------------------------------------------"
 
 		self.population = sorted(self.population, key=lambda gene: gene.fitness)
 		self.error = 1 - self.fittest().fitness
 
-	def select(self):
-		# Keep the population size constant
-		self.population = self.population[-self.popsize:]
 
-	def breed(self):
+	def crossover(self):
 		offsprings = []
 		for gene in self.population:
 			# Breed with random gene from the population
@@ -70,7 +82,25 @@ class GeneticAlgorithm:
 
 		self.population = self.population + offsprings
 
+	def breed(self, parents):
+		# Create a copy from one of the genes
+		offspring = copy.deepcopy(parents[0])
+
+		n = len(parents) - 1
+		for i, chromosome in enumerate(offspring.genotype):
+			chromosome = parents[randint(0, n)][i]
+
+			offspring.mutate()
+			offspring.decode()
+
+			return offspring
+
+	def select(self):
+		# Keep the population size constant
+		self.population = self.population[-self.popsize:]
+
 	def roulette(self):
+		# TODO: Implement Roulette selection
 		pass
 
 	def fittest(self):
