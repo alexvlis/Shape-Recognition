9734c2cc01f7f61711dd8153ff78afaf7058c8e4
alex <alex.vlissidis@gmail.com>
2016-11-28 18:42:56 +0000

    Vectorization

--- neuralnet.py
+++ neuralnet.py
@@ -1,82 +1,64 @@
+import cPickle
 import numpy as np
 from genetics import Gene
-from nnmath import *
-import cStringIO
-
-class ErrorMinimized(Exception):
-	def __init__(self, message):
-		self.message = message
-
-
-class Neuron:
-	def __init__(self, size, activation):
-		self.w = 5 * (2 * np.random.random_sample(size) - 1)
-		self.b = 1 * (2 * np.random.random_sample() - 1)
-		self.activation = activation
-
-	def activate(self, inputs):
-		return self.activation(np.dot(self.w, inputs) + self.b)
-
 
 class NeuralNet(Gene):
-	layers = []
-
-	def __init__(self, args):
+	def __init__(self, args, build=True):
 		(skeleton, activation) = args
-		self.build(skeleton, activation)
+		self.layers = []
+		self.activation = activation
+		self.skeleton = skeleton
+		self.cursor = 0
 
-	def build(self, skeleton, activation):
+		if build:
+			self.build(skeleton)
+			self.encode()
+
+	def build(self, skeleton):
 		for i, width in enumerate(skeleton[1:], start=1):
-			layer = [Neuron(size=skeleton[i-1], activation=activation) for _ in range(width)]
-			self.layers.append(layer)
+			weights = 5 * (2 * np.random.sample((skeleton[i-1], width)) - 1)
+			biases = 5 * (2 * np.random.sample(width) - 1)
+			self.layers.append((weights, biases))
 
-	def feed_forward(self, input_vector):
-		for layer in self.layers:
-			outputs = []
-			for neuron in layer:
-				outputs = np.append(outputs, neuron.activate(input_vector))
+	def feed_forward(self, input_vec):
+		for (weights, biases) in self.layers:
+			input_vec = self.activation(np.dot(input_vec, weights) + biases)
 
-			input_vector = outputs
-
-		return outputs
+		return input_vec
 
 	def load(self, filename):
-		"""
-		This method sets the parameters of the neural net from file and assumes
-		it has been built with the correct dimensions.
-		"""
+		'''This method sets the parameters of the neural net from file.'''
 		with open(filename, "r") as f:
-			for layer in self.layers:
-				for neuron in layer:
-					line = f.readline().split(";")
-					neuron.w = np.array(line[0:-1]).astype(np.float)
-					neuron.b = float(line[-1])
+			self.layers = cPickle.loads(f.read())
 
 	def save(self, filename):
 		with open(filename, "w+") as f:
-		    for layer in self.layers:
-		        for neuron in layer:
-					for weight in neuron.w:
-					    f.write(str(weight) + ";")
-					f.write(str(neuron.b) + "\n")
+			f.write(cPickle.dumps(self.layers))
 
 
 	'''*********************** Overload gene methods ************************'''
 
 	def encode(self):
-		for layer in self.layers:
-			for neuron in layer:
-				for weight in neuron.w:
-					self.genotype += float_to_bin(weight)
-				self.genotype += float_to_bin(neuron.b)
+		genotype = np.array([]) # Initialise a new genotype
+		for weights, biases in self.layers:
+			genotype = np.concatenate((genotype, weights.flatten(), biases))
+
+		self.genotype = genotype
 
 	def decode(self):
-		genotype = cStringIO(self.genotype)
-		for layer in self.layers:
-			for neuron in layer:
-				for weight in neuron.w:
-					weight = bin_to_float(genotype.read(64))
-				neuron.b = bin_to_float(genotype.read(64))
+		for i, width in enumerate(self.skeleton[1:], start=1):
+			d = skeleton[i-1] * width
+			weights = self.read_genotype(d).reshape(self.skeleton[i-1], width)
+			biases = self.read_genotype(width)
+
+			self.layers.append((weight, biases))
+
+		self.cursor = 0 # Reset the cursor
 
 	def evaluate(self, input_vector):
 		return self.feed_forward(input_vector)
+
+	def read_genotype(self, delta):
+		chunk = self.genotype[self.cursor:self.cursor + delta]
+		self.cursor += delta
+		return chunk
