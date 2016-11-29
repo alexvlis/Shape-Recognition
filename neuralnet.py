e329b8fd37fba88b7acf74ff972f35aef06b2307
alex <alex.vlissidis@gmail.com>
2016-11-29 15:45:59 +0000

    Removed read_genome

--- neuralnet.py
+++ neuralnet.py
@@ -4,24 +4,21 @@ from genetics import Gene
 
 class NeuralNet(Gene):
 	def __init__(self, args, build=True):
-		(skeleton, activation) = args
 		self.layers = []
-		self.activation = activation
-		self.skeleton = skeleton
-		self.cursor = 0
+		(self.skeleton, self.activation) = args
 
 		if build:
-			self.build(skeleton)
+			self.build(self.skeleton)
 			self.encode()
 
 	def build(self, skeleton):
 		for i, width in enumerate(skeleton[1:], start=1):
-			weights = 5 * (2 * np.random.sample((skeleton[i-1], width)) - 1)
+			weights = 100 * (2 * np.random.sample((skeleton[i-1], width)) - 1)
 			biases = 5 * (2 * np.random.sample(width) - 1)
 			self.layers.append((weights, biases))
 
 	def feed_forward(self, input_vec):
-		for (weights, biases) in self.layers:
+		for weights, biases in self.layers:
 			input_vec = self.activation(np.dot(input_vec, weights) + biases)
 
 		return input_vec
@@ -32,11 +29,12 @@ class NeuralNet(Gene):
 			self.layers = cPickle.loads(f.read())
 
 	def save(self, filename):
+		'''This method saves the parameters of the neural net to file.'''
 		with open(filename, "w+") as f:
 			f.write(cPickle.dumps(self.layers))
 
 
-	'''*********************** Overload gene methods ************************'''
+	'''*********************** Overload Gene methods ************************'''
 
 	def encode(self):
 		genotype = np.array([]) # Initialise a new genotype
@@ -47,18 +45,13 @@ class NeuralNet(Gene):
 
 	def decode(self):
 		for i, width in enumerate(self.skeleton[1:], start=1):
-			d = skeleton[i-1] * width
+			d = self.skeleton[i-1] * width
 			weights = self.read_genotype(d).reshape(self.skeleton[i-1], width)
 			biases = self.read_genotype(width)
 
-			self.layers.append((weight, biases))
+			self.layers.append((weights, biases))
 
 		self.cursor = 0 # Reset the cursor
 
 	def evaluate(self, input_vector):
 		return self.feed_forward(input_vector)
-
-	def read_genotype(self, delta):
-		chunk = self.genotype[self.cursor:self.cursor + delta]
-		self.cursor += delta
-		return chunk
