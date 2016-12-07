e18d15bff05a39b3cf74b6fe1bae067b055e202b
alex <alex.vlissidis@gmail.com>
2016-12-07 00:24:49 +0000

    New evaluation method

--- genetics.py
+++ genetics.py
@@ -1,6 +1,10 @@
 import numpy as np
 from random import randint
 
+class GAKill(Exception):
+	def __init__(self, message):
+		self.message = message
+
 # Gene Super Class
 class Gene:
 	fitness = 0
@@ -22,9 +26,12 @@ class Gene:
 
 	def mutate(self, rate):
 		gen_len = len(self.genotype)
-                # Select some random chromosomes
-                idx = np.random.random_integers(0, gen_len-1, size=(1, round(rate*gen_len)))
-                self.genotype[idx] += 2 * np.random.random_sample(1) - 1 # Add a small -/+ number
+
+		# Select some random chromosomes
+		idx = np.random.random_integers(0, gen_len-1, size=(1, round(rate*gen_len)))
+
+		# Add a small -/+ number
+		self.genotype[idx] += 2 * np.random.random_sample(1) - 1
 
 	def read_genotype(self, delta):
 		chunk = self.genotype[self.cursor:self.cursor + delta]
@@ -33,6 +40,9 @@ class Gene:
 
 
 class GeneticAlgorithm:
+	popsize = 0
+	error = 1
+
 	def __init__(self, error, mutation_rate, data, targets, obj, args):
 		"""
 		This contructor takes multiple parameters as well as the constructor
@@ -45,8 +55,6 @@ class GeneticAlgorithm:
 		self.target_error = error
 		self.training_data = data
 		self.targets = targets
-		self.popsize = 0
-		self.error = 1
 
 	def populate(self, size):
 		# Use the object constructor to create the population
@@ -58,52 +66,27 @@ class GeneticAlgorithm:
 
 	def evaluate(self):
 		for gene in self.population:
-			# Reset the score
-			gene.score = 0
-			for data in self.training_data:
-				(tags, input_vector) = data
-				output = gene.evaluate(input_vector)
-				#print output
-
-				# Determine the desired output
-				target = map(lambda x: int(x in tags), self.targets)
-
-				# Calculate how well it performed
-				eval_vector = (target == np.around(output)).astype(np.int)
-
-				# Build the scoring vector
-				scorer = [0] * len(output)
-				for i, value in enumerate(target):
-					if value == 0:
-						scorer[i] = 1 - output[i]
-					else:
-						scorer[i] = output[i]
-
-				gene.score += np.dot(scorer, eval_vector)
-
-			gene.fitness = gene.score/(len(eval_vector) * len(self.training_data))
+			gene.evaluate(self.training_data, self.targets, 10)
 
 		self.population = sorted(self.population, key=lambda gene: gene.fitness)
-		self.error = 1 - self.fittest().fitness  # Set the error
+		self.error = self.fittest().error  # Set global the error
 
 	def crossover(self):
-		offsprings = []
-
-		# Select parents based on roulette selection
-		for _ in range(self.popsize):
-			offsprings.append(self.breed(self.roulette(2)))
-
-		self.population = np.array(offsprings)
+		# Create new population using roulette selection and breeding
+		self.population = [self.breed(self.roulette(2)) for _ in range(self.popsize)]
 
 	def breed(self, parents):
-		# Make a new gene and don't update global population size
+		# Make a new gene
 		offspring = self.singleton()
 
-		# Determine points of cut
+		# Determine cutting points
 		length = parents[0].genotype.size - 1
-		cuts = [randint(0, length/2), randint(length/2, length)]
+		cuts = [randint(0, round(length/2)), randint(round(length/2), length)]
+
 		# Perform 2-point crossover
+		# TODO: Implement random order of breeding
 		offspring.genotype = np.concatenate((parents[0].genotype[:cuts[0]], parents[1].genotype[cuts[0]:cuts[1]], parents[0].genotype[cuts[1]:]))
+
 		offspring.mutate(self.mutation_rate)
 		offspring.decode()
 
